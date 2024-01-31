7efe5d908608d550927be71ea84a910e7c907a69
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-31 17:25:55 +0100

    end of wednesday

--- node/index.js
+++ node/index.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import express from 'express'
+import Meteo from './models/Meteo.js'
 
 let Mongo_USERNAME=process.env.Mongo_USERNAME
 let Mongo_PASSWORD=process.env.Mongo_PASSWORD
@@ -8,6 +9,8 @@ let Mongo_DB=process.env.Mongo_DB
 
 mongoose.connect(`mongodb+srv://${Mongo_USERNAME}:${Mongo_PASSWORD}@introduction.mcajypg.mongodb.net/${Mongo_DB}`)
 
+const report = await Meteo.findById("65ba6b5a76e007eeb8ecf34a").exec()
+console.log(report)
 
 const app = express();
 const PORT = process.env.PORT || 4111;
