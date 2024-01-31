74f49b90f254cbda9561a59f7546f9a63bce8ed0
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-31 16:53:26 +0100

    Modif: using import instead of require(type: module) and added environnement variable DB

--- node/index.js
+++ node/index.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import express from 'express'
 
-let Mongo_USERNAME=config.env.Mongo_USERNAME
-let Mongo_PASSWORD=config.env.Mongo_PASSWORD
+let Mongo_USERNAME=process.env.Mongo_USERNAME
+let Mongo_PASSWORD=process.env.Mongo_PASSWORD
+let Mongo_DB=process.env.Mongo_DB
 
-mongoose.connect(`mongodb+srv://${Mongo_USERNAME}:${Mongo_PASSWORD}@introduction.mcajypg.mongodb.net/`)
+mongoose.connect(`mongodb+srv://${Mongo_USERNAME}:${Mongo_PASSWORD}@introduction.mcajypg.mongodb.net/${Mongo_DB}`)
 
-const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 4111;
 app.listen(PORT, console.log("Server running on port: " + PORT))
