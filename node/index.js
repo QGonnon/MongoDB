65be6ccc1a409443b31298485124375d9d94fa44
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-31 16:50:57 +0100

    done adding connection with environnement variable

--- node/index.js
+++ node/index.js
@@ -1,3 +1,11 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+let Mongo_USERNAME=config.env.Mongo_USERNAME
+let Mongo_PASSWORD=config.env.Mongo_PASSWORD
+
+mongoose.connect(`mongodb+srv://${Mongo_USERNAME}:${Mongo_PASSWORD}@introduction.mcajypg.mongodb.net/`)
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4111;
