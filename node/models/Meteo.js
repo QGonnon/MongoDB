c017adc4ad75ca967b15ca24befb5a26f8c74acc
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-31 16:54:25 +0100

    init meteo model

---
+++ node/models/Meteo.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const meteoSchema = new Schema({
+  date: String,
+  tmax: Number,
+  tmin: Number,
+  tavg: Number,
+  departure: Number,
+  HDD: Number,
+  CDD: Number,
+  precipitation: Number,
+  new_snow: Number,
+  snow_depth: Number,
+});
+
+const Meteo = model('meteo', meteoSchema);
+export default Meteo;
